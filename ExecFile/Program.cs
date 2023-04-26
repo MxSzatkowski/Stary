ac2045115f68c24f42b5057daf45cfcc889e185d
Michał Szatkowski <michal.szatkowski@vp.pl>
2023-04-26 08:42:58 +0200

    i

---
+++ ExecFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using ExecFile;
+using System.Threading.Tasks;
+using Wrapper;
+
+namespace Test
+{
+    public class Exec
+    {
+        static void Main(string[] args)
+        {
+            var tets = new ExecWrapper();
+            tets.UseWrapperMethod();
+        }
+    }
+}
---
+++ UnitTests/Program.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using FakeItEasy;
+using Wrapper;
+using NUnit.Framework.Internal.Execution;
+using Test;
+using ExecFile;
+using System.Runtime.InteropServices;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public const string DllPath = @"..\..\..\..\x64\Debug\CPlusPlus.dll";
+
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int AddNumbers();
+
+
+        [Test]
+        public void UseAddNumber_return30()
+        {
+            var wrapperContext = new CPlusPlusWraper();
+
+            var c = 30;
+
+            var result = wrapperContext.UseAddNumbers();
+
+            Assert.That(result, Is.EqualTo(c));
+        }
+
+    }
+}
